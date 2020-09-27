ad6131ebe3b4c4068a1ebafc3dd6faa8b3e3767a
connorberek226 <64981067+connorberek226@users.noreply.github.com>
2020-09-26 19:37:53 -0500

    Successful bargraph; need to fix the axes

--- static/js/app.js
+++ static/js/app.js
@@ -1,48 +1,57 @@
 // Fetch the JSON data and console log it
 // d3.json("samples.json").then((data) => console.log(data));
-
-d3.json("data/samples.json").then(function(data) {
-    console.log(data);
-})
+d3.json("data/samples.json").then((data) => {
+    console.log(data.samples[0]);
+});
 
 function buildChart(sample) {
     
     
-    d3.json("data/samples.json").then((data) =>{
-            
-        function filterSampleData(otu) {
-            return otu.samples;
-        }
-        const barChartData = data.filter(filterSampleData);
+    
+    d3.json(`data/samples.json`).then((data) => {
+        
+        // console.log(data);
 
-        console.log(barChartData);
-        var sampleArray = [];
-        for (let i = 0;i < barChartData.length; i++) {
-            sampleArray.push({
-                "otu_ids": barChartData.otu_ids[i],
-                "sample_values": barChartData.sample_values[i],
-                "otu_labels": barChartData.otu_labels[i]            
+        console.log(data.samples);
+
+        
+
+        let barChartData = [];
+        for (let i = 0; i < data.samples.length; i++) {
+            barChartData.push({
+                "otu_ids": data.samples[i]["otu_ids"],
+                "sample_values": data.samples[i]["sample_values"],
+                "otu_labels": data.samples[i]["otu_labels"]
             })
         }
-        
-        
-        const sampleArraySorted = sampleArray.sort((a, b) => 
+        console.log(barChartData);
+        const barChartDataSorted = barChartData.sort((a, b) => 
             parseFloat(b.sample_values) - parseFloat(a.sample_values)
         );
-        console.log(sampleArraySorted);
+        // console.log(barChartDataSorted);
+
+        const topIDS = barChartDataSorted.map(d => d.otu_ids).slice(0,10);
+        const topSampleValues = barChartDataSorted.map(d => d.sample_values).slice(0,10);
+        const otuLabels = barChartDataSorted.map(d => d.otu_labels).slice(0,10);
+        // console.log(topIDS);
+        // console.log(topSampleValues);
+        // console.log(otuLabels)
         
         const barChart = [{
-            labels: sampleArraySorted.map(data => data.otu_ids).slice(0,10),
-            values: sampleArraySorted.map(data => data.sample_values).slice(0,10),
+            x: topSampleValues,
+            y: topIDS,
             type: "bar",
-            name: sampleArraySorted.map(data => data.otu_labels).slice(0,10),
+            orientation: "h",
+            text: otuLabels
         }];
         
         const layout = {
             title: `Top 10 OTU from sample ${sample}`
         };
-        const barPlot = document.getElementById("bar");
-        Plotly.newPlot(barPlot, barChart, layout);
+        
+        Plotly.newPlot("bar", barChart, layout);
+
+      
 
 
 
@@ -50,4 +59,32 @@ function buildChart(sample) {
 
 }
 
-console.log(buildChart())
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+  
+    // Use the list of sample names to populate the select options
+    d3.json("data/samples.json").then((sampleNames) => {
+        
+        sampleNames.names.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+      console.log(sampleNames.names[0]);
+      // Use the first sample from the list to build the initial plots
+      const firstSample = sampleNames.names[0];
+      buildChart(firstSample);
+      
+    });
+}
+
+
+function optionChanged(newSample) {
+    // Fetch new data each time a new sample is selected
+    buildChart(newSample);
+    buildMetadata(newSample);
+ }
+  
+init();
