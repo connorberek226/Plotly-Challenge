fcf0b3df8c6369c2d6589fbc4e828e90a80b75f9
connorberek226 <64981067+connorberek226@users.noreply.github.com>
2020-09-26 22:05:26 -0500

    Bubble Data graph added, not portraying any data yet though

--- static/js/app.js
+++ static/js/app.js
@@ -4,39 +4,37 @@ d3.json("data/samples.json").then((data) => {
     console.log(data.samples[0]);
 });
 
+   
+
 function buildChart(sample) {
-    
-    
-    
+
     d3.json(`data/samples.json`).then((data) => {
         
-        // console.log(data);
-
-        console.log(data.samples);
-
+        console.log(data.samples[sample]);
         
-
         let barChartData = [];
         for (let i = 0; i < data.samples.length; i++) {
             barChartData.push({
+                "id": data.samples[i]["id"],
                 "otu_ids": data.samples[i]["otu_ids"],
                 "sample_values": data.samples[i]["sample_values"],
                 "otu_labels": data.samples[i]["otu_labels"]
             })
         }
         console.log(barChartData);
-        const barChartDataSorted = barChartData.sort((a, b) => 
-            parseFloat(b.sample_values) - parseFloat(a.sample_values)
+        
+        const barChartDataSorted = barChartData.sort((a, b) =>
+            parseInt(b.sample_values) - parseInt(a.sample_values)
         );
-        // console.log(barChartDataSorted);
+        console.log(barChartDataSorted);
 
-        const topIDS = barChartDataSorted.map(d => d.otu_ids).slice(0,10);
-        const topSampleValues = barChartDataSorted.map(d => d.sample_values).slice(0,10);
-        const otuLabels = barChartDataSorted.map(d => d.otu_labels).slice(0,10);
+        const topIDS = barChartDataSorted.map(d => d.otu_ids).slice(0, 10);
+        const topSampleValues = barChartDataSorted.map(d => d.sample_values).slice(0, 10);
+        const otuLabels = barChartDataSorted.map(d => d.otu_labels).slice(0, 10);
         // console.log(topIDS);
         // console.log(topSampleValues);
         // console.log(otuLabels)
-        
+
         const barChart = [{
             x: topSampleValues,
             y: topIDS,
@@ -44,14 +42,42 @@ function buildChart(sample) {
             orientation: "h",
             text: otuLabels
         }];
-        
+
         const layout = {
             title: `Top 10 OTU from sample ${sample}`
         };
-        
+
         Plotly.newPlot("bar", barChart, layout);
 
-      
+
+
+        var bubbleData = [{
+            x: data.samples["otu_ids"],
+            y: data.samples["sample_values"],
+            text: data.samples["otu_labels"],
+            type: "scatter",
+            mode: "markers",
+            marker: {
+                color: data.samples["otu_ids"],
+                symbol: "circle",
+                size: data.samples["sample_values"],
+            },
+        }];
+        var bubbleLayout = {
+            title: `<b>Biodiversity of sample ${sample}</b>`,
+            xaxis: {
+                title: "OTU ID",
+                // range: [startDate, endDate],
+            },
+            yaxis: {
+                title: "Value",
+                // autorange: true,
+                range: [0, Math.max(data.samples["sample_values"])]
+                // type: "linear"
+            }
+        };
+
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
 
 
@@ -62,21 +88,21 @@ function buildChart(sample) {
 function init() {
     // Grab a reference to the dropdown select element
     var selector = d3.select("#selDataset");
-  
+
     // Use the list of sample names to populate the select options
     d3.json("data/samples.json").then((sampleNames) => {
-        
+
         sampleNames.names.forEach((sample) => {
-        selector
-          .append("option")
-          .text(sample)
-          .property("value", sample);
-      });
-      console.log(sampleNames.names[0]);
-      // Use the first sample from the list to build the initial plots
-      const firstSample = sampleNames.names[0];
-      buildChart(firstSample);
-      
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+        console.log(sampleNames.names[0]);
+        // Use the first sample from the list to build the initial plots
+        const firstSample = sampleNames.names[0];
+        buildChart(firstSample);
+
     });
 }
 
@@ -85,6 +111,6 @@ function optionChanged(newSample) {
     // Fetch new data each time a new sample is selected
     buildChart(newSample);
     buildMetadata(newSample);
- }
-  
+}
+
 init();
